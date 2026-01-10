e05f9255887811d61cd59dedb4b5131889f248d4
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 21:20:33 -0500

    Create RoomC.ts

---
+++ src/scenes/RoomC.ts
@@ -0,0 +1,22 @@
+import Phaser from "phaser";
+import { questState } from "../state/QuestState";
+
+export default class RoomC extends Phaser.Scene {
+  constructor() { super("RoomC"); }
+
+  preload() {
+    this.load.image("roomC", "/rooms/roomC.png");
+    this.load.image("starsage", "/sprites/npc-starsage.png");
+  }
+
+  create() {
+    this.add.image(0, 0, "roomC").setOrigin(0);
+
+    const sage = this.add.sprite(300, 300, "starsage").setInteractive();
+
+    sage.on("pointerdown", () => {
+      questState.complete();
+      window.location.href = "/api/mint";
+    });
+  }
+}
