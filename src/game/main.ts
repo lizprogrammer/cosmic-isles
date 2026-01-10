77175c32c76503d17016e43e60492f81ef76d303
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 21:47:22 -0500

    Update main.ts

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,13 +1,4 @@
-import Phaser from "phaser";
-import AvatarCreator from "./scenes/AvatarCreator";
-import RoomA from "./scenes/RoomA";
-import RoomB from "./scenes/RoomB";
-import RoomC from "./scenes/RoomC";
+import Phaser from 'phaser'
+import { config } from './config'
 
-new Phaser.Game({
-  type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  parent: "game",
-  scene: [AvatarCreator, RoomA, RoomB, RoomC],
-});
+new Phaser.Game(config)
