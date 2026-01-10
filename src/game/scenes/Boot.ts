e3341b6f0e3009864bbe166074cc2e26e4af2301
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 21:44:41 -0500

    Create Boot.ts

---
+++ src/game/scenes/Boot.ts
@@ -0,0 +1,15 @@
+import Phaser from 'phaser'
+
+export class Boot extends Phaser.Scene {
+  constructor() {
+    super('Boot')
+  }
+
+  preload() {
+    // preload minimal assets here if needed
+  }
+
+  create() {
+    this.scene.start('AvatarCreator')
+  }
+}
