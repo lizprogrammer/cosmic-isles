2dc5c3d4fee3e84804aa5923f991602e1ede0b88
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-10 13:21:42 -0500

    Update config.ts

--- src/game/config.ts
+++ src/game/config.ts
@@ -7,8 +7,13 @@ import RoomC from './scenes/RoomC'
 
 export const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
+  parent: 'game',
   width: 800,
   height: 600,
   backgroundColor: '#000000',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
   scene: [Boot, AvatarCreator, RoomA, RoomB, RoomC]
 }
