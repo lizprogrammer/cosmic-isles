4bc9ae09ffd36a086253ce6763ad499cda7df4f2
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 23:09:02 -0500

    Update page.tsx

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store"
+export const revalidate = 0
+
 import { useEffect } from "react"
 import Phaser from "phaser"
 import { config } from "../../game/config"
