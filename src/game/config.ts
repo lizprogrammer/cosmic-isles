cc035379e226c174d35f83690d0d31c1254f66fa
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 21:45:08 -0500

    Create config.ts

---
+++ src/game/config.ts
@@ -0,0 +1,14 @@
+import Phaser from 'phaser'
+import { Boot } from './scenes/Boot'
+import { AvatarCreator } from './scenes/AvatarCreator'
+import { RoomA } from './scenes/RoomA'
+import { RoomB } from './scenes/RoomB'
+import { RoomC } from './scenes/RoomC'
+
+export const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#000000',
+  scene: [Boot, AvatarCreator, RoomA, RoomB, RoomC]
+}
