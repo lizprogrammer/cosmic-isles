606badaafc85fafce7e7262c78598a40ef9971a1
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 22:58:20 -0500

    Update QuestState.ts

--- src/game/state/QuestState.ts
+++ src/game/state/QuestState.ts
@@ -1,11 +1,16 @@
-export const questState = {
+export interface QuestState {
+  completed: boolean
+  complete: () => void
+}
+
+export const questState: QuestState = {
   completed: false,
 
   complete() {
-    this.completed = true;
+    this.completed = true
     fetch("/api/progress", {
       method: "POST",
       body: JSON.stringify({ questCompleted: true }),
-    });
+    })
   },
-};
+}
