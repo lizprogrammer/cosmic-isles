cc6ad943450de92a4c98509486a3846c7165f1dc
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-09 22:57:37 -0500

    Update PlayerState.ts

--- src/game/state/PlayerState.ts
+++ src/game/state/PlayerState.ts
@@ -1,6 +1,13 @@
-export const playerState = {
+export interface PlayerState {
+  hasStone: boolean
+  bodyColor: string
+  outfit: string
+  accessory: string | null
+}
+
+export const playerState: PlayerState = {
   hasStone: false,
   bodyColor: "base-blue",
   outfit: "outfit1",
-  accessory: null,
-};
+  accessory: null
+}
