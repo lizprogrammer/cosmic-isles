77835e9fe65e32114904795929f59acf648c68d6
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-10 14:09:56 -0500

    Update config.ts

--- src/game/config.ts
+++ src/game/config.ts
@@ -13,7 +13,18 @@ export const config: Phaser.Types.Core.GameConfig = {
   backgroundColor: '#000000',
   scale: {
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600,
+    // Add these for better mobile support
+    min: {
+      width: 320,
+      height: 240
+    },
+    max: {
+      width: 1920,
+      height: 1440
+    }
   },
   scene: [Boot, AvatarCreator, RoomA, RoomB, RoomC]
 }
