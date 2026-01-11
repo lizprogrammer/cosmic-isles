f8eb16b7a76683da0538fb259df53f0f606c87ac
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-10 21:30:28 -0500

    Create head.tsx

---
+++ src/app/game/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="fc:frame" content="vNext" />
+      <meta name="fc:frame:image" content="https://cosmic-isles.vercel.app/splash.png" />
+      <meta name="fc:frame:button:1" content="Play Game" />
+      <meta name="fc:frame:button:1:action" content="link" />
+      <meta name="fc:frame:button:1:target" content="https://cosmic-isles.vercel.app/" />
+    </>
+  )
+}
