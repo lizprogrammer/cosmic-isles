4af1549cfa26005d1a3bee606c50ea5203dc7aae
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-10 19:48:14 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,37 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // Fix Phaser + browser-only build
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
       path: false,
-      crypto: false
+      crypto: false,
+      stream: false,
+      buffer: false
     };
     return config;
   },
 
-  // Allow importing TS/JS from /src/game
   experimental: {
     esmExternals: "loose"
-  },
-
-  async redirects() {
-    return [
-      {
-        source: '/:path((?!another-page$).*)',
-        has: [{ type: 'header', key: 'x-redirect-me' }],
-        permanent: false,
-        destination: '/another-page',
-      },
-      {
-        source: '/:path((?!another-page$).*)',
-        missing: [{ type: 'header', key: 'x-do-not-redirect' }],
-        permanent: false,
-        destination: '/another-page',
-      },
-    ];
-  },
+  }
 };
 
 module.exports = nextConfig;
