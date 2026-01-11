f6e3a50eca3c706bc2617f36d509f14d12a067c7
Elizabeth Rosso <32004735+lizprogrammer@users.noreply.github.com>
2026-01-10 19:52:04 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,14 +8,25 @@ const nextConfig = {
       path: false,
       crypto: false,
       stream: false,
-      buffer: false
+      buffer: false,
     };
     return config;
   },
 
   experimental: {
-    esmExternals: "loose"
-  }
+    esmExternals: "loose",
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination:
+          "https://api.farcaster.xyz/miniapps/hosted-manifest/019baa46-22b9-d5c5-be37-a3f72b063671",
+        permanent: false, // 307
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
